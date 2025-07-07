5c8a0f287be32a7df3a83c671076731615bd514b
GabrielaGalmes <galmesgabriela@gmail.com>
2025-07-07 03:35:38 -0300

    Update productos.js
    Corrección

--- js/productos.js
+++ js/productos.js
@@ -7,11 +7,11 @@ export async function getProducts(category = 'mens-shoes', limit = 5) {
         const datos = await respuesta.json();
         // Mapeo de datos de la API a esta estructura
         return datos.products.map(item => ({
-            id: item.id, // ID del producto
-            name: item.title || 'Producto sin nombre', // Nombre del producto
-            price: item.price || 0, // Precio del producto
-            thumbnail: item.thumbnail || 'placeholder.jpg', // Imagen del producto
-            description: item.description || 'Sin descripción' // descripcion del producto
+            id: item.id, 
+            name: item.title || 'Producto sin nombre', 
+            price: item.price || 0, 
+            thumbnail: item.thumbnail || 'placeholder.jpg', 
+            description: item.description || 'Sin descripción' 
         }));
     } catch (error) {
         console.error("Error al obtener productos:", error);
